91dc425d85c4962d012df2232b0558836c344e00
evjohnston <evj@stanford.edu>
2024-12-19 11:33:32 -0500

    starting with arpanet

--- timeline-data.js
+++ timeline-data.js
@@ -1,27 +1,13 @@
 // Timeline data store
 const timelineData = [
     {
-        date: '1988-01-01',
-        title: 'Wide Area Information Servers (WAIS)',
-        content: 'Thinking Machines Inc. develops a new Internet protocol dubbed WAIS. Users can download a WAIS client to search through an array of distributed servers and access the documents stored there. The protocol supports fuzzy searching, custom tailored results, and decentralized data storage, but ultimately loses out to the web.',
-        relatedArticle: null
+        date: '1969-10-29',
+        title: 'The Birth of ARPANET',
+        content: 'The Advanced Research Projects Agency Network (ARPANET) was the first wide-area packet-switched network with distributed control and one of the first computer networks to implement the TCP/IP protocol suite. ARPANET connected four universities: UCLA, Stanford, UC Santa Barbara, and the University of Utah. The first message sent over the network, "LO" (intended to be "LOGIN"), marks the beginning of people using networked systems to share data.',
+        relatedArticle: {
+            url: 'https://en.wikipedia.org/wiki/ARPANET#:~:text=The%20first%20permanent%20ARPANET%20link,four%2Dnode%20network%20was%20established.',
+            title: "Wikipedia - ARPANET",
+            excerpt: "The ARPANET was established by the Advanced Research Projects Agency (now DARPA) of the United States Department of Defense. Learn more on Wikipedia!"
+        }
     },
-    {
-        date: '1989-03-12',
-        title: 'Information Management, a Proposal',
-        content: 'While working at CERN, Tim Berners-Lee first comes up with the idea for the World Wide Web. To pitch it, he submits a proposal for organizing scientific documents to his employers titled "Information Management, a Proposal." In this proposal, Berners-Lee sketches out what the web will become, including early versions of the HTTP protocol and HTML.',
-        relatedArticle: null
-    },
-    {
-        date: '1990-10-17',
-        title: 'IMDb',
-        content: 'Col Needham publishes a few Unix scripts to a Usenet group for browsing and searching through a user generated index of movie lists subdivided into several categories. He calls it the rec.arts.movies movies database. Years later, Needham and a few others would move the interface online and incorporate officially as IMDb.',
-        relatedArticle: null
-    },
-    {
-        date: '1990-12-25',
-        title: 'WorldWideWeb Browser',
-        content: 'Tim Berners-Lee releases WorldWideWeb (later Nexus) on Christmas day, the first ever browser for the web. It is far from primitive, featuring a built-in HTML editor alongside graphical features. However, it is only available on NeXT machines and fails to gain much traction.',
-    }
-    // Add more entries following the same structure
 ];
